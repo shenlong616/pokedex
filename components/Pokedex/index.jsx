db734d8b2908585bfd90b68884fa36b1028254c6
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-10-25 20:59:01 +0700

    test

--- components/Pokedex/index.jsx
+++ components/Pokedex/index.jsx
@@ -7,6 +7,7 @@ import { InView } from "react-intersection-observer";
 import Head from "next/head";
 import { settings } from "../../pokedex.config";
 import { Transition } from "@headlessui/react";
+import Tilt from "react-parallax-tilt";
 
 export default function () {
   const [state, setState] = useState({
@@ -74,58 +75,60 @@ export default function () {
                             enterFrom="opacity-0 translate-y-2/3 skew-y-12"
                             enterTo="opacity-100 translate-y-0 skew-y-0"
                           >
-                            <div
-                              className={clsx(
-                                [
-                                  "grid scale-90 cursor-pointer grid-cols-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 transition duration-150 ease-in-out hover:scale-100",
-                                ],
-                                {
-                                  "hover:shadow-xl hover:shadow-green-500":
-                                    getHighestElement === 0,
-                                },
-                                {
-                                  "hover:shadow-xl hover:shadow-red-500":
-                                    getHighestElement === 1,
-                                },
-                                {
-                                  "hover:shadow-xl hover:shadow-blue-500":
-                                    getHighestElement === 2,
-                                }
-                              )}
-                            >
-                              <Image
-                                src={sprites.front_default}
-                                width={"100%"}
-                                height={"100%"}
-                                alt={species.name}
-                                // onLoadingComplete={() => {}}
-                              />
-                              <div className="self-center">
-                                <Stat>
-                                  {[
-                                    array[0],
-                                    "bg-green-500",
-                                    (stats[0].base_stat / totalArray) * 100,
-                                  ]}
-                                </Stat>
-                                <div className="my-0.5">
+                            <Tilt tiltMaxAngleX={9} tiltMaxAngleY={3}>
+                              <div
+                                className={clsx(
+                                  [
+                                    "grid scale-90 cursor-pointer grid-cols-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 transition duration-150 ease-in-out hover:scale-100",
+                                  ],
+                                  {
+                                    "hover:shadow-xl hover:shadow-green-500":
+                                      getHighestElement === 0,
+                                  },
+                                  {
+                                    "hover:shadow-xl hover:shadow-red-500":
+                                      getHighestElement === 1,
+                                  },
+                                  {
+                                    "hover:shadow-xl hover:shadow-blue-500":
+                                      getHighestElement === 2,
+                                  }
+                                )}
+                              >
+                                <Image
+                                  src={sprites.front_default}
+                                  width={"100%"}
+                                  height={"100%"}
+                                  alt={species.name}
+                                  // onLoadingComplete={() => {}}
+                                />
+                                <div className="self-center">
                                   <Stat>
                                     {[
-                                      array[1],
-                                      "bg-red-500",
-                                      (stats[1].base_stat / totalArray) * 100,
+                                      array[0],
+                                      "bg-green-500",
+                                      (stats[0].base_stat / totalArray) * 100,
+                                    ]}
+                                  </Stat>
+                                  <div className="my-0.5">
+                                    <Stat>
+                                      {[
+                                        array[1],
+                                        "bg-red-500",
+                                        (stats[1].base_stat / totalArray) * 100,
+                                      ]}
+                                    </Stat>
+                                  </div>
+                                  <Stat>
+                                    {[
+                                      array[2],
+                                      "bg-blue-500",
+                                      (stats[2].base_stat / totalArray) * 100,
                                     ]}
                                   </Stat>
                                 </div>
-                                <Stat>
-                                  {[
-                                    array[2],
-                                    "bg-blue-500",
-                                    (stats[2].base_stat / totalArray) * 100,
-                                  ]}
-                                </Stat>
                               </div>
-                            </div>
+                            </Tilt>
                             <small className="font-mono font-medium">
                               {name}
                             </small>
