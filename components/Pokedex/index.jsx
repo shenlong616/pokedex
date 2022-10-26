37b624ed1f6a17caecfb97386d6fa0e70f8a2b88
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-10-26 09:52:33 +0700

    test

--- components/Pokedex/index.jsx
+++ components/Pokedex/index.jsx
@@ -82,19 +82,19 @@ export default function () {
                                     "grid scale-90 cursor-pointer grid-cols-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 transition duration-150 ease-in-out hover:scale-100 hover:animate-pulse",
                                   ],
                                   {
-                                    "hover:shadow-xl hover:shadow-green-500":
+                                    "hover:shadow-xl hover:shadow-green-500 active:ring-1 active:ring-green-500 active:ring-offset-2 active:ring-offset-green-500":
                                       getHighestElement === 0,
                                   },
                                   {
-                                    "hover:shadow-xl hover:shadow-red-500":
+                                    "hover:shadow-xl hover:shadow-red-500 active:ring-1 active:ring-red-500 active:ring-offset-2 active:ring-offset-red-500":
                                       getHighestElement === 1,
                                   },
                                   {
-                                    "hover:shadow-xl hover:shadow-blue-500":
+                                    "hover:shadow-xl hover:shadow-blue-500 active:ring-1 active:ring-blue-500 active:ring-offset-2 active:ring-offset-blue-500":
                                       getHighestElement === 2,
                                   }
                                 )}
-                                onClick={() => alert(`${id} - ${name} `)}
+                                // onClick={() => alert(`${id} - ${name} `)}
                               >
                                 <Image
                                   src={sprites.front_default}
