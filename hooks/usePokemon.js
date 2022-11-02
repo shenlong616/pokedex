325f4df2539ced1af08b63dd3cf8175580353960
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-11-02 11:55:51 +0700

    test

--- hooks/usePokemon.js
+++ hooks/usePokemon.js
@@ -1,11 +1,14 @@
 // https://swr.vercel.app/docs/getting-started#make-it-reusable
 import useSWR from "swr";
+import axios from "axios";
+
 import { settings } from "../pokedex.config";
 
 export default function usePokemon(parameter) {
   const { data, error } = useSWR(
     `${settings.api}/pokemon/${parameter}`,
-    (...args) => fetch(...args).then((res) => res.json())
+    // https://swr.vercel.app/docs/data-fetching#axios
+    (parameter) => axios.get(parameter).then((res) => res.data)
   );
 
   return {
