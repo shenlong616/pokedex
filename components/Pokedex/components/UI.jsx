005bda162f2b5cbc15747ac28c224aef063fe4c9
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-11-04 14:18:15 +0700

    test

--- components/Pokedex/components/UI.jsx
+++ components/Pokedex/components/UI.jsx
@@ -5,19 +5,21 @@ function UI() {
   return null;
 }
 
-UI.pokemonImage = ({ children }) => (
-  <div className="-scale-x-100 select-none">{children}</div>
-);
+UI.pokemonImage = function ({ children }) {
+  return <div className="-scale-x-100 select-none">{children}</div>;
+};
 
-UI.pokemonName = ({ children }) => (
-  <div
-    className={clsx(
-      ["select-all text-xs font-medium"],
-      [settings.style.text.color[0]]
-    )}
-  >
-    {children}
-  </div>
-);
+UI.pokemonName = function ({ children }) {
+  return (
+    <div
+      className={clsx(
+        ["select-all text-xs font-medium"],
+        [settings.style.text.color[0]]
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default UI;
