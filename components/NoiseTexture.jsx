a48c4182fa9fa63794266659abf6b8fbeacc84cf
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-11-04 17:04:33 +0700

    test

--- components/NoiseTexture.jsx
+++ components/NoiseTexture.jsx
@@ -1,7 +1,7 @@
 // https://github.com/delbaoliveira/website/blob/main/ui/Layout.tsx#L40-L58
 export default function () {
   return (
-    <svg className="pointer-events-none fixed isolate z-50 flex min-h-full min-w-full flex-col mix-blend-color-burn">
+    <svg className="pointer-events-none fixed isolate z-50 flex h-full w-full flex-col mix-blend-color-burn">
       <filter id="pedroduarteisalegend">
         <feTurbulence
           type="fractalNoise"
