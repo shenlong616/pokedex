23bb31994e8acb564297adf765a1329b11b5db50
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-11-05 17:42:51 +0700

    test

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -7,7 +7,7 @@ export default function ({ children }) {
     <Slideout prop1="bottom">
       <footer
         className={clsx(
-          "flex flex-col items-center font-mono font-medium",
+          "flex flex-col items-center py-2 font-mono font-medium",
           settings.style.background.footer
         )}
       >
