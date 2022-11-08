96825eec49014b77ecb1a8aad9ee155d5bfa7115
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2022-11-08 08:20:08 +0700

    test

--- pokedex.config.js
+++ pokedex.config.js
@@ -8,10 +8,10 @@ export const settings = {
   text: {
     CloseButtonText: [
       <span>
-        Click <span className="text-slate-200">here</span> to close the dialog.
+        <span className="text-slate-200">Click here</span> to close the dialog.
       </span>,
       <span>
-        Or click <span className="text-slate-200">outside</span> the dialog to
+        <span className="text-slate-200">Or click outside</span> the dialog to
         close it.
       </span>,
     ],
